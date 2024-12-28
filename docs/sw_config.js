0dfc7ce760f757b98bdcfbc06f5b0967537792a1
locker98 <coolemail45@proton.me>
2024-12-27 21:57:23 -0500

    Update sw_config.js

--- docs/sw_config.js
+++ docs/sw_config.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-   swetrix.init('iZsltLSgC1NF', {  apiURL: 'https://asw.locker98.com/log'})
+   swetrix.init('cKC1una3CDaJ', {  apiURL: 'https://asw.locker98.com/log'})
    swetrix.trackViews()
 })
 
